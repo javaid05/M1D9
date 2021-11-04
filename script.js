1fbc15749b85c31b0fe566d04714f95fb9a4b44a
javaid05 <javaidmalik.khan@gmail.com>
2021-11-04 15:55:25 +0000

    display random number , also display ramdon number in bingo card

--- script.js
+++ script.js
@@ -9,6 +9,30 @@ const generateBingoNumber = function (num) {
 };
 generateBingoNumber(76);
 
+let numberGenerate = [];
+
+const randomNumberDisplay = document.getElementById("displayNumber");
+let randomNumber = Math.ceil(Math.random() * 76);
+randomNumberDisplay.innerHTML = `<p>${randomNumber}</p>`;
+numberGenerate.push(randomNumber);
 window.onload = function () {
   generateBingoNumber();
+  highlightNumber(randomNumber);
 };
+let bingoNumbers = document.getElementsByClassName("bingoNumber");
+let userNumbers = document.getElementsByClassName("userNumber");
+
+let highlightNumber = function (number) {
+  for (let bingonum of bingoNumbers) {
+    if (bingonum.innerText == number) {
+      bingonum.classList.add("highlighted");
+    }
+  }
+};
+for (let userNum of userNumbers) {
+  if (userNum.innerText == number) {
+    userNum.classList.add("highlighted");
+  }
+}
+let buttonClick = document.getElementById("numberGenerator");
+buttonClick.addEventListener("click", generateBingoNumber);
